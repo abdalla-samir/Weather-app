d8ac6ad11cb725fcb46a7d13e7bdcbb644d93d10
Abdalla Samir <abdallasamir860@gmail.com>
2024-09-16 00:09:59 +0300

    Weather app first release

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useRef, useState } from 'react';
 
 function App() {
+  let [input, setInput] = useState("0");
+  let [degree, setDegree] = useState(0);
+  let [name, setName] = useState("Country Name")
+  useEffect(() => {
+    fetch(`http://api.weatherapi.com/v1/current.json?key=051e6170314d47cc9cc165511243108&q=${input}&aqi=no`)
+      .then((data) => {
+        return data.json();
+      }).then((data) => {
+        setDegree(data.current.temp_c)
+        setName(data.location.name);
+      }).catch((error) => {
+        setDegree("Location Not Found");
+      })
+  }, [input]);
+  let inputData = useRef();
+  let prevent = (event) => {
+    event.preventDefault();
+  }
+  let handleChanges = (e) => {
+    setInput(inputData.current.value);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='App-logo'>
+        <img src='logo.png' alt='' />
+      </div>
+      <div className='inputs'>
+        <form onSubmit={prevent}>
+          <input className='in-one' type='text' name='country' placeholder='Country Name' ref={inputData} />
+          <input className='in-two' type='submit' onClick={handleChanges} value='Results' />
+        </form>
+      </div>
+      <div className='results'>
+        <h2>{name}</h2>
+        <div className='weather-result'>
+          {degree + " Degree"}
+        </div>
+      </div>
     </div>
   );
 }
-
 export default App;
